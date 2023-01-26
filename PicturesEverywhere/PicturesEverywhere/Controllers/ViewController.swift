10e2846b8e4b7f76532378f6004e571bafeda0bf
Ezequiel Rasgido <ezequielrasgido@icloud.com>
2023-01-26 20:04:00 -0300

    Initial scaffolding

---
+++ PicturesEverywhere/PicturesEverywhere/Controllers/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  PicturesEverywhere
+//
+//  Created by Ezequiel Rasgido on 26/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
